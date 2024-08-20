960e0fae3a3b885c8c874fd3f2248cb0262fa86c
0122345 <ntwarifiacre043@gmail.com>
2024-08-20 16:09:59 +0200

    removing unwanted assets

--- flutter/crazy_todoapp/lib/components/homepage.dart
+++ flutter/crazy_todoapp/lib/components/homepage.dart
@@ -12,26 +12,24 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            child: Container(
-              margin: EdgeInsets.zero,
-              width: double.infinity,
-              //height:
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/Gojo satoru art 4k wallpaper.jpeg'),
-                  fit: BoxFit.cover,
-                ),
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/Yuta.jpeg'),
+                fit: BoxFit.cover,
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: MediaQuery.of(context).size.height / 2,
             child: Container(
-              width: 420,
-              height: 320,
               decoration: const BoxDecoration(
                 color: Color.fromARGB(255, 221, 224, 226),
                 borderRadius: BorderRadius.only(
@@ -39,87 +37,91 @@ class _HomepageState extends State<Homepage> {
                   topRight: Radius.circular(20.0),
                 ),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      'Manage',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30.0,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  const Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      'your',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30.0,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10.0),
-                  const Align(
-                    alignment: AlignmentDirectional.centerEnd,
-                    child: Padding(
-                      padding: EdgeInsets.only(right: 18.0),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 18.0, top: 12),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Align(
+                      alignment: Alignment.topLeft,
                       child: Text(
-                        'tasks',
+                        'Manage',
                         style: TextStyle(
-                            fontSize: 60.0,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 206, 198, 198)),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 56.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.all(12.0),
-                        child: Text(
-                          'Get started',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.black,
-                          ),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30.0,
+                          color: Colors.black,
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 18.0),
-                        child: FloatingActionButton(
-                          shape: const CircleBorder(
-                            side: BorderSide(
-                              color: Color.fromARGB(255, 133, 130, 130),
-                              width: 3.0,
+                    ),
+                    const Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'your',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10.0),
+                    const Align(
+                      alignment: AlignmentDirectional.centerEnd,
+                      child: Padding(
+                        padding: EdgeInsets.only(right: 18.0),
+                        child: Text(
+                          'tasks',
+                          style: TextStyle(
+                              fontSize: 60.0,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 206, 198, 198)),
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.all(12.0),
+                          child: Text(
+                            'Get started',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.black,
                             ),
                           ),
-                          backgroundColor: Colors.black,
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const GeneralTaskPage(),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 18.0),
+                          child: FloatingActionButton(
+                            shape: const CircleBorder(
+                              side: BorderSide(
+                                color: Color.fromARGB(255, 133, 130, 130),
+                                width: 3.0,
                               ),
-                            );
-                          },
-                          child: const Icon(
-                            Icons.arrow_forward,
-                            color: Colors.white,
+                            ),
+                            backgroundColor: Colors.black,
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const GeneralTaskPage(),
+                                ),
+                              );
+                            },
+                            child: const Icon(
+                              Icons.arrow_forward,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                    const SizedBox(height: 20.0),
+                  ],
+                ),
               ),
             ),
           ),
