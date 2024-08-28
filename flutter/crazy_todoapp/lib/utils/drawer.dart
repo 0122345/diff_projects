cf3c060466c4a4b704436bc69a5ddb3b12a5b6fa
0122345 <ntwarifiacre043@gmail.com>
2024-08-28 17:33:50 +0200

    SideBar UI

--- flutter/crazy_todoapp/lib/utils/drawer.dart
+++ flutter/crazy_todoapp/lib/utils/drawer.dart
@@ -1,3 +1,4 @@
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
 import 'package:get/get.dart';
@@ -24,9 +25,169 @@ class MenuScreen extends GetView<MyDrawerController> {
   const MenuScreen({super.key});
 
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return Container(
-      color: Colors.amber,
+       
+      color: const Color.fromARGB(255, 212, 218, 218),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(left: 12.0),
+                      child: Text(
+                        'Settings',
+                        style: TextStyle(
+                          fontFamily: 'Matemasie',
+                          fontSize: 22,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: IconButton(
+                        onPressed: () {
+                          // Get.back();
+                        },
+                        icon: const Icon(Icons.settings_suggest_outlined),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 24),
+                Container(
+                  width: 100,
+                  height: 5,
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              
+              const SizedBox(height: 24),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            'theme Settings',
+                            style: GoogleFonts.poppins(
+                              fontSize: 16,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          const Icon(Icons.light_mode,
+                           size: 20,
+                           color: Color.fromARGB(255, 0, 187, 201),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 24),
+                       Row(
+                        children: [
+                          Text('about us',
+                           style: GoogleFonts.poppins(
+                              fontSize: 16,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                             ),
+                          ),
+                          const SizedBox(width: 20),
+                          const Icon(Icons.info,
+                           size: 20,
+                           color: Color.fromARGB(255, 0, 187, 201),
+                          ),
+                        ],
+                      ),
+                       Row(
+                        children: [
+                          Text('help',
+                           style: GoogleFonts.poppins(
+                            fontSize: 16,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                           ),
+                          ),
+                          const SizedBox(width: 10),
+                          const Icon(Icons.help_outline,
+                          size: 20,
+                          color: Color.fromARGB(255, 0, 187, 201),
+                          ),
+                        ],
+                      ),
+                       Row(
+                        children: [
+                          Text('logout',
+                           style: GoogleFonts.poppins(
+                            fontSize: 16,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                           ),
+                          ),
+                          const SizedBox(width: 10),
+                           const Icon(Icons.logout,
+                           size: 20,
+                           color: Color.fromARGB(255, 0, 187, 201),
+                          ),
+                        ],
+                      ),
+                        Row(
+                        children: [
+                          Text('privacy policy',
+                           style: GoogleFonts.poppins(
+                             fontSize: 16,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                           ),
+                          ),
+                          const SizedBox(width: 10),
+                          const Icon(Icons.lock,
+                           size: 20,
+                           color: Color.fromARGB(255, 0, 187, 201),
+                          ),
+                        ],
+                      ),
+                       Row(
+                        children: [
+                          Text('terms and conditions',
+                           style: GoogleFonts.poppins(
+                            fontSize: 16,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                           ),
+                          ),
+                          const SizedBox(width: 10),
+                          const Icon(Icons.gavel,
+                           size: 20,
+                           color: Color.fromARGB(255, 0, 187, 201),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
