d07100b089d40365bcf09dac9f173d06671629ed
0122345 <ntwarifiacre043@gmail.com>
2025-05-08 16:38:45 +0200

    little modification

---
+++ flutter/todo/lib/view/screen_usage.dart
@@ -0,0 +1,3 @@
+//TODO: screen usage: bar dia, animation, theme, ios friendly
+
+import 'package:flutter/material.dart';
---
+++ flutter/todo/lib/widget/calendar_view_w.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:calendar_slider/calendar_slider.dart';
+
+class SecondHomeScreenVi extends StatefulWidget {
+  const SecondHomeScreenVi({super.key});
+
+  @override
+  State<SecondHomeScreenVi> createState() => _SecondHomeScreenViState();
+}
+
+class _SecondHomeScreenViState extends State<SecondHomeScreenVi> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Column(children: [
+           _buildHeader(context),
+           _buildCalendar(),
+        ],
+      ));
+  }
+}
+
+Widget _buildHeader(BuildContext context) {
+  return Column(
+    children: [
+      Row(
+        children: [
+          Text(
+            'Focus Score',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+
+          SizedBox(width: 20),
+
+          Container(
+            width: 28,
+            height: 30,
+            decoration: BoxDecoration(shape: BoxShape.circle),
+            child: Icon(Icons.person, size: 30),
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+Widget _buildCalendar() {
+  return Container(
+    child: Column(
+      children: [
+        Text('$DateTime.now()'),
+        Container(
+          width: double.infinity,
+          height: 200,
+          decoration: BoxDecoration(),
+          child: CalendarSlider(
+            initialDate: DateTime.now(),
+            firstDate: DateTime.now().subtract(const Duration(days: 20)),
+            lastDate: DateTime.now().add(const Duration(days: 30)),
+            onDateSelected: (date) {
+              if (kDebugMode) {
+                print('selected date: $date');
+              }
+            },
+          ),
+        ),
+      ],
+    ),
+  );
+}
